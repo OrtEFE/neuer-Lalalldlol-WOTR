15be010a2fb35aa7e4a40647d5d1935ba3241222
OrtEFE <97741275+OrtEFE@users.noreply.github.com>
2022-06-10 15:15:14 +0200

    Clone Trooper

---
+++ js/config.js
@@ -0,0 +1,128 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/wotrlogo01.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "Warriors Of The Republic";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/touhou01.png",
+	"backgrounds/images/touhou02.png",
+	"backgrounds/images/touhou03.png",
+	"backgrounds/images/touhou04.png",
+	"backgrounds/images/touhou05.jpg",
+	"backgrounds/images/touhou05.jpg",
+	"backgrounds/images/touhou06.jpg",
+	"backgrounds/images/touhou07.jpg",
+	"backgrounds/images/touhou08.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = false;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/headup.ogg", name: "Star Wars - Head Up AMV"},
+	{ogg: "songs/playwithfire.ogg", name: "Star Wars - Play With Fire AMV"},
+	{ogg: "songs/firethescore.ogg", name: "Star Wars - Fire AMV"},
+	{ogg: "songs/bringthafire.ogg", name: "Star Wars - Bring That Fire AMV"},
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Willkommen auf Warriors Of The Republic!",
+"Wir wünschen dir Viel Spaß auf Warriors Of The Republic",
+"Danke fürs Spielen auf Warriors Of The Republic!",
+"Halte dich an die Regeln!",
+"Schau doch gerne mal bei unserem Discord vorbei!"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
